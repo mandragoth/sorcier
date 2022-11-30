62cebd6d82c857a1856534d2e4957b9cd9b28612
Enalye <enalyen@gmail.com>
2022-11-30 01:12:00 +0100

    Update .gitmodules, dub.json, and 20 more files...

---
+++ source/sorcier/script/primitive.d
@@ -0,0 +1,33 @@
+module sorcier.script.primitive;
+
+import magia, grimoire;
+
+import sorcier.script.util;
+
+void loadMagiaLibPrimitive(GrLibrary library) {
+    GrType colorType = grGetClassType("Color");
+    library.addFunction(&_rectangle1, "rectangle", [
+            grFloat, grFloat, grFloat, grFloat
+        ]);
+    library.addFunction(&_rectangle2, "rectangle", [
+            grFloat, grFloat, grFloat, grFloat, colorType
+        ]);
+    library.addFunction(&_rectangle3, "rectangle", [
+            grFloat, grFloat, grFloat, grFloat, colorType, grFloat
+        ]);
+}
+
+private void _rectangle1(GrCall call) {
+    drawFilledRect(Vec2f(call.getFloat(0), call.getFloat(1)),
+        Vec2f(call.getFloat(2), call.getFloat(3)));
+}
+
+private void _rectangle2(GrCall call) {
+    drawFilledRect(Vec2f(call.getFloat(0), call.getFloat(1)),
+        Vec2f(call.getFloat(2), call.getFloat(3)), toColor(call.getObject(4)));
+}
+
+private void _rectangle3(GrCall call) {
+    drawFilledRect(Vec2f(call.getFloat(0), call.getFloat(1)),
+        Vec2f(call.getFloat(2), call.getFloat(3)), toColor(call.getObject(4)), call.getFloat(5));
+}
